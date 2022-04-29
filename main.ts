90b0a028b35d6344b0e44727998e0216c9e358c3
JohnFabu <102181948+JohnFabu@users.noreply.github.com>
2022-04-29 11:25:54 +0200

    pocitadlo

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
-let pocitadlo = 3
-basic.forever(function () {
+let pocitadlo = 0
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(0)
+})
+input.onButtonPressed(Button.A, function () {
+    pocitadlo = 5
     for (let index = 0; index <= pocitadlo; index++) {
         basic.showNumber(pocitadlo - index)
     }
+    music.playMelody("C D E F G A B C5 ", 120)
+})
+basic.forever(function () {
+	
 })
